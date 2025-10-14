c2224912f61446d614bba73f32056b03b3a16402
kiran k <59312528+thekiran@users.noreply.github.com>
2025-10-14 15:02:07 +0530

    .

--- apps/web/database/redis.ts
+++ apps/web/database/redis.ts
@@ -4,20 +4,20 @@
 //   url: process.env.UPSTASH_REDIS_REST_URL!,
 //   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 // });
- 
+
 // export default redis;
 
-import Redis from 'ioredis';
+import Redis from "ioredis";
 
 // Check if REDIS_URL is defined
 const redisUrl = process.env.REDIS_URL;
 
 if (!redisUrl) {
-  throw new Error('REDIS_URL environment variable is not defined');
+  throw new Error("REDIS_URL environment variable is not defined");
 }
 
 // The ioredis client can parse the full connection string (REDIS_URL)
-const redis = new Redis(redisUrl); 
+const redis = new Redis(redisUrl);
 
 redis.on("error", (err) => console.log("Redis Client Error", err));
 
